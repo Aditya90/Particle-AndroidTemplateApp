2cb6bb3e19453f0348d354e390419f1321c446d5
Aditya Sreekumar <asreekumar@lutron.com>
2015-10-08 13:02:55 -0400

    - Create a new activity and view to show device properties - Bug in passing extras

--- app/src/main/java/com/asdesign/sparkapptest/DeviceConfigActivity.java
+++ app/src/main/java/com/asdesign/sparkapptest/DeviceConfigActivity.java
@@ -1,13 +1,119 @@
 package com.asdesign.sparkapptest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import io.particle.android.sdk.cloud.ParticleCloud;
+import io.particle.android.sdk.cloud.ParticleCloudException;
+import io.particle.android.sdk.cloud.ParticleDevice;
+import io.particle.android.sdk.utils.Async;
+import io.particle.android.sdk.utils.Toaster;
 
 public class DeviceConfigActivity extends AppCompatActivity {
 
+    // Get the passed in device name we chose
+    Intent passedInIntent = getIntent();
+    String nameDevice = passedInIntent.getStringExtra("ParticleDevice");
+
+    ParticleCloud localSparkCloud;
+    ParticleDevice localParticleDevice;
+
+    ListView listView;
+
+    private final static String PARTICLE_DEVICE_TAG = "SPARK_TEST_LOG_TAG";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i(PARTICLE_DEVICE_TAG, "onCreate()");
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_device_config);
+
+        // Retrieve the particle device for the passed in name
+        initDevice(findViewById(R.id.deviceConfigView));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_logged_in, menu);
+        return true;
+    }
+
+    @Override
+    protected void onResume() {
+        Log.i(PARTICLE_DEVICE_TAG, "onResume()");
+
+        super.onResume();
+        setContentView(R.layout.activity_logged_in);
+
+        // Display the properties of the device
+        listDeviceProps();
+    }
+
+    /**
+     * @brief This function retrieves the list of connected devices from the cloud
+     * @param myView specifies the view from which this is being called
+     */
+    private void initDevice(View myView){
+
+        // List out all connected devices
+        Async.executeAsync(localSparkCloud, new Async.ApiWork<ParticleCloud, List<ParticleDevice>>() {
+
+            public List<ParticleDevice> callApi(ParticleCloud particleCloud)
+                    throws ParticleCloudException, IOException {
+                return particleCloud.getDevices();
+            }
+
+            @Override
+            public void onSuccess(List<ParticleDevice> devices) {
+                for (ParticleDevice device : devices) {
+                    if (device.getName().equals(nameDevice)) {
+                        localParticleDevice = device;
+                        return;
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(ParticleCloudException e) {
+                //Log.e("SOME_TAG", e);
+                Log.i(PARTICLE_DEVICE_TAG, "Call onFailure()");
+                Toaster.l(DeviceConfigActivity.this, "Wrong credentials or no internet connectivity, please try again");
+            }
+        });
+    }
+
+    private void listDeviceProps(){
+        // Get the list view object
+        listView = (ListView) findViewById(R.id.deviceConfigListView);
+
+        // Copy the names of all devices to an array
+        List<String> localParticleDeviceString = new ArrayList<String>();
+
+        localParticleDeviceString.add(localParticleDevice.getName().toString());
+        localParticleDeviceString.add(localParticleDevice.getID().toString());
+        localParticleDeviceString.add(localParticleDevice.getVersion().toString());
+
+        // Define a new Adapter
+        // First parameter - Context
+        // Second parameter - Layout for the row
+        // Third parameter - ID of the TextView to which the data is written
+        // Forth - the Array of data
+        ArrayAdapter<String> localSparkDevicesAdapter = new ArrayAdapter<String>(DeviceConfigActivity.this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, localParticleDeviceString);
+
+        listView.setAdapter(localSparkDevicesAdapter);
     }
 }
